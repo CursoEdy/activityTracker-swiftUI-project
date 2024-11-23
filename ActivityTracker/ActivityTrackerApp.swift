9e7727e4b61d37570f0270c095a554f78bd3c6c0
edy alves <30.edyalves@gmail.com>
2024-11-23 18:05:24 -0300

    Initial Commit

---
+++ ActivityTracker/ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerApp.swift
+//  ActivityTracker
+//
+//  Created by ednardo alves on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
