a2458d5c5c30464b01522399f5a781cb9ea5f629
edy alves <30.edyalves@gmail.com>
2024-11-23 19:24:07 -0300

    sectorMark, Pie chart and Donut Chart

--- ActivityTracker/ActivityTracker.swift
+++ ActivityTracker/ActivityTracker.swift
@@ -15,15 +15,57 @@ struct ActivityTracker: View {
     
     @Environment(\.modelContext) private var contect
     
+    @State private var newName: String = ""
+    @State private var hoursPerDay: Double = 0
+    @State private var currentActivity: Activity? = nil
+    
+    @State private var selectCount: Int?
+    
     var body: some View {
         NavigationStack {
             
             VStack {
-                Text("Activity Tracker")
+                Chart{
+                    
+                    let isSelected: Bool = true
+                    
+                    ForEach(activites) {activite in
+                        SectorMark(
+                            angle: .value("Value", activite.HoursPerDay),
+                            innerRadius: .ratio(0.6),
+                            outerRadius: isSelected ? 1.0 : .ratio(0.95),
+                            angularInset: 1
+                        ).foregroundStyle(.red.opacity(0.5))
+                            .cornerRadius(10)
+                    }
+                    
+//                    SectorMark(
+//                        angle: .value("Value", 5),
+//                        innerRadius: .ratio(0.6),
+//                        outerRadius: .ratio(0.95),
+//                        angularInset: 1
+//                    ).foregroundStyle(.red.opacity(0.5))
+//                        .cornerRadius(10)
+//                    SectorMark(
+//                        angle: .value("Value", 2),
+//                        innerRadius: .ratio(0.6),
+//                        outerRadius: .ratio(0.95),
+//                        angularInset: 1
+//                    ).foregroundStyle(.green.opacity(0.5))
+//                        .cornerRadius(10)
+//                    SectorMark(
+//                        angle: .value("Value", 10),
+//                        innerRadius: .ratio(0.6),
+//                        outerRadius: .ratio(1.0),
+//                        angularInset: 1
+//                    ).foregroundStyle(.blue.opacity(0.5))
+//                        .cornerRadius(10)
+                }
             }
             .padding()
             .navigationTitle("Activity Tracker")
         }
+        
     }
 }
 
